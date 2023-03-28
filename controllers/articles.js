2d13a20c9e8e6ec3db46b69b847f2c79583ceb04
Scott Dyer <scottdyer@Scotts-MacBook-Air.local>
2023-03-28 10:39:27 +0100

    Completed GET /api/articles/:article_id and testing. Set up error handling middleware

---
+++ controllers/articles.js
@@ -0,0 +1,13 @@
+const app = require("../db/app");
+const { fetchArticleById } = require('../models/articles')
+
+exports.getArticleById = (req, res, next) => {
+    const { article_id } = req.params;
+    fetchArticleById(article_id).then((rows) =>{
+        const article = rows;
+        res.status(200).send({ article })
+    })
+    .catch((err) => {
+        next(err)
+    })
+};
